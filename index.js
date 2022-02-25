7f91cea32d2975fb0a4459b0335c81ae8bfbafc8
Morikachan <zuzukochkame@gmail.com>
2022-02-25 10:32:32 +0100

    Syntax fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const readlineSync = require("readline-sync");
 const txt = process.argv[2];
 
 if (txt) {
-    console.log("Non-interactive mode on.\n");
+    console.log("Non-Interactive mode on.");
 
     let file = fs.readFileSync(txt, "utf8");
     parceFile(file);
@@ -79,13 +79,13 @@ function equationSolver(coefs) {
     console.log(`\nEquation is: ${a}x^2 + ${b}x + ${c}`);
     const discriminant = Math.pow(b, 2) - 4 * a * c;
 
-    if (discriminant < 0) return console.log("The equation does not have roots.");
+    if (discriminant < 0) return console.log("The equation has no roots.");
     if (discriminant > 0) {
         let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
         let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
         if (x1 === x2) {
             console.log(`There's one root: ${x1}\n`);
-        } else console.log(`There's two roots:\nx1 = ${x1}\nx2 = ${x2}\n`);
+        } else console.log(`There're two roots:\nx1 = ${x1}\nx2 = ${x2}\n`);
     } else {
         let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
         console.log(`There's one root: ${x1}`);
